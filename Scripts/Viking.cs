769d63e586ac71e775fcaca24c1fcd5c6a01ae00
Wuselwog <30385654+Wuselwog@users.noreply.github.com>
2017-07-26 21:35:35 +0200

    FixedAnimBugsOfViking
    The major animation bugs of the Viking are now fixed. Animations: Walk, hit, grab_start, grabbed, fly

---
+++ Scripts/Viking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viking : Creature {
+
+	// Use this for initialization
+	void Start () {
+        maxHealth = 100;
+        creat_start();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        creat_update();
+    }
+
+
+
+}
